26f9b6a8f6600603320d69585b1cfec137cc0b16
Habibe Bayram <112430387+habibyrm@users.noreply.github.com>
2024-11-04 14:39:01 +0300

    Add project files.

---
+++ web4.2/Controllers/OgrenciController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web4._2.Models;
+
+namespace web4._2.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OgrEkle(Ogrenci ogr) 
+        {
+            return View();
+        }
+        public IActionResult IndexOto()
+        {
+            return View();
+        }
+    }
+}
---
+++ web4.2/Models/Ogrenci.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web4._2.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name ="Öğrenci Ad")]
+        [Required(ErrorMessage ="Öğrenci adını giriniz!")]
+        public string OgrAd {  get; set; }
+        [Display(Name = "Öğrenci Soyad")]
+        [Required(ErrorMessage = "Öğrenci soyadını giriniz!")]
+        public string OgrSoyad { get; set; }
+        [Display(Name = "Öğrenci Numarası")]
+        [Required(ErrorMessage = "Öğrenci numarasını giriniz!")]
+        public string OgrNo { get; set; }
+        [Display(Name = "Öğrenci Yaşı")]
+        [Required(ErrorMessage = "Öğrenci yaşını giriniz!")]
+        [Range(18,65,ErrorMessage ="Yaş aralığı 18-65 olmalıdır")]
+        public int OgrYas { get; set; }
+        [Display(Name = "Öğrencinin Doğum Tarihi")]
+        [Required(ErrorMessage = "Öğrenci doğum tarihini giriniz!")]
+        public DateTime OgrDTarih { get; set; }
+    }
+}
